2812475efc9f3cbe328596c820c8557884703399
jang <jang@ioctl.org>
2020-12-29 14:59:30 +0000

    Day 3 part a

--- src/Day3.hs
+++ src/Day3.hs
@@ -16,14 +16,30 @@ import Lib
 
 
 {-
+--- Day 3: Squares With Three Sides ---
+
+Now that you can think clearly, you move deeper into the labyrinth of hallways and office furniture that makes up this part of Easter Bunny HQ. This must be a graphic design department; the walls are covered in specifications for triangles.
+
+Or are they?
+
+The design document gives the side lengths of each triangle it describes, but... 5 10 25? Some of these aren't triangles. You can't help but mark the impossible ones.
+
+In a valid triangle, the sum of any two sides must be larger than the remaining side. For example, the "triangle" given above is impossible, because 5 + 10 is not larger than 25.
+
+In your puzzle input, how many of the listed triangles are possible?
 -}
 
+parse :: [String] -> [(Integer, Integer, Integer)]
 parse ls = ls
-         & head
-         & splitOn "-"
-         & map read
+         & map (\l -> splitOn " " l & filter (/= ""))
+         & map (\[a,b,c] -> (read a, read b, read c))
 
-day3 ls = "hello world"
+orient (a,b,c) = let a' = minimum [a,b,c]
+                     c' = maximum [a,b,c]
+                     b' = (a + b + c) - a' - c'
+                 in (a', b', c')
+
+day3 ls = parse ls & map orient & filter (\(a,b,c) -> a + b > c) & length
 
 {-
 -}
